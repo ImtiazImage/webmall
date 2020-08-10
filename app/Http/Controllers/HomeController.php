fe385d270c001998dbca95ce708104e9ea6cfb8f
unknown <smimtiaz.cse@gmail.com>
2020-08-11 03:07:44 +0600

    know about laravel default pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class HomeController extends Controller
 {
     public function index() {
-        return view('pages.index');
+        $posts = DB::table('posts')
+                ->join('categories','posts.category_id','categories.id')
+                ->select('posts.*','categories.name','categories.slug')->paginate(2);
+        return view('pages.index',compact('posts'));
     }
 
     public function about() {
